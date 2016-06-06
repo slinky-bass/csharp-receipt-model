21a6c606030bc10f689e21e8ab48b1d19f78f2b3
slinky-bass <slinky_bass@yahoo.co.uk>
2016-06-06 21:29:58 +0200

    initial commit

---
+++ Receipt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receipt
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Cashier c1 = new Cashier() {
+                id = 1,
+                name = "Bob"
+            };
+			
+			Cashier c2 = new Cashier() {
+                id = 2,
+                name = "Jack"
+            };
+
+            Console.WriteLine(String.Format("Employee name: {0}, Employee id: {1}", c1.name, c1.id));
+            //Console.WriteLine("Employee name: {0}, Employee id: {1}", c1.name, c1.id);
+            
+            StoreSale sale1 = StoreSale.NewSale(c1);
+            Console.WriteLine(String.Format("SaleNumber: {0}, Cashier: {1}", sale1.CreatedAt.ToString("dd/MM/yyyy hh:mm:ss.fff"), sale1.CurrentCashier.name));
+            
+            StoreSale sale2 = StoreSale.NewSale(c2);
+            Console.WriteLine(String.Format("SaleNumber: {0}, Cashier: {1}", sale2.CreatedAt.ToString("dd/MM/yyyy hh:mm:ss.fff"), sale2.CurrentCashier.name));
+            Console.ReadLine();
+		}
+	}
+}
---
+++ Receipt/StoreSale.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receipt
+{
+	/// <summary>
+	/// Description of StoreSale.
+	/// </summary>
+	public class StoreSale
+	{
+		private DateTime _CreatedAt;
+		private Cashier _CurrentCashier;
+		
+		public DateTime CreatedAt
+		{
+			get
+			{
+				//return what ever _CurrentCashier is set to
+				return _CreatedAt;
+			} 
+			set 
+			{
+				//Set the field to whatever value is passed in
+				_CreatedAt = value;
+			}
+		}
+        
+		public Cashier CurrentCashier
+		{
+			get
+			{
+				//return what ever _CurrentCashier is set to
+				return _CurrentCashier;
+			} 
+			set 
+			{
+				//Set the field to whatever value is passed in
+				_CurrentCashier = value;
+			}
+		}
+
+        public static StoreSale NewSale(Cashier NewCashier) {
+        	StoreSale sale = new StoreSale();
+        	sale.CreatedAt = DateTime.Now;
+            sale.CurrentCashier = NewCashier;
+
+            //Console.WriteLine("CreatedAt: {0}, CurrentCashier: {1}", CreatedAt, CurrentCashier.name);
+            //Console.ReadLine();
+            
+            return sale;
+        }
+	}
+}
